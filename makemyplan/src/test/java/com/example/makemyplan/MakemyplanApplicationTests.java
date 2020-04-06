19e1e19b6ff613c2f203dc769708ad3f21d8ff0b
sandeep-dev02 <devoppsaws2020@gmail.com>
2020-04-06 18:38:38 +0530

    codeRefactor

---
+++ makemyplan/src/test/java/com/example/makemyplan/MakemyplanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.makemyplan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakemyplanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
